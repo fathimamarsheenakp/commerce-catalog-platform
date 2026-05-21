a7b6ae1ebb1ae818b3309c7aae37052747bd566c
Marsheena <fathima.marsheena@sony.com>
2026-05-21 16:15:17 +0530

    dockerize frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://api-gateway:8080',
         changeOrigin: true,
       },
     },
